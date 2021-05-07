114c5d2de4dcdf3dfe46d20ac6bec8b29de9175e
中村遼太 <nkmr94317rt@gmail.com>
2021-05-07 11:57:15 +0900

    basic sample

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -73,7 +73,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
       body: Column(
         children: [
           TableCalendar(
-            //locale: 'ja_JP',
+            locale: 'ja_JP',
             firstDay: DateTime.utc(2020, 1, 1),
             lastDay: DateTime.utc(2030, 12, 31),
             focusedDay: _focusedDay,
